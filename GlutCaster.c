83cacc02bcb9eb9de6b616ee9ddc12f125d474e7
DrTressi <DrTressi@yandex.ru>
2025-08-23 01:15:35 +0700

    feat: empty source file added

---
+++ GlutCaster.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+
+int WIDTH = 1280;
+int HEIGHT = 720;
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+void init()
+{
+	glClearColor(.3,.3,.3,0);
+	gluOrtho2D(0,1024,512,0);
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Glut Caster Test 2");
+	init();
+	
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
