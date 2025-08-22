f7b0ac073afa36f1e8763782c3ccb51072e53b32
MingulovTA <mingulov.t@yandex.ru>
2025-08-23 03:00:33 +0700

    feat: raycast uncompleted

--- GlutCaster.c
+++ GlutCaster.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
 
-int WIDTH = 1280;
-int HEIGHT = 720;
+#define PI 3.1415926535
+#define P2 PI/2
+#define P3 3*PI
 
-float px, py; //Player position
+int WIDTH = 1024;
+int HEIGHT = 512;
+
+float px, py, pdx,pdy, pa; //Player position
 
 void drawPlayer()
 {
@@ -13,6 +18,12 @@ void drawPlayer()
 	glPointSize(8);
 	glBegin(GL_POINTS);
 	glVertex2i(px,py);
+	glEnd();
+	
+	glLineWidth(3);
+	glBegin(GL_LINES);
+	glVertex2i(px,py);
+	glVertex2i(px+pdx*5, py+pdy*5);
 	glEnd();
 }
 
@@ -59,20 +70,175 @@ void drawMap2D()
 	}
 }
 
+float dist(float ax, float ay, float bx, float by, float ang)
+{
+	return (sqrt((bx-ax)*(bx-ax)+(by-ay)*(by-ay)));
+}
+
+void drawRays3D()
+{
+	int r, mx, my, mp, dof;
+	float rx, ry,ra,xo,yo, disV, disH;
+	ra = pa;
+	for (r=0;r<1;r++)
+	{
+		//----------------------Check Horisontal Lines----------------------
+		dof = 0;
+		float disH = 1547800;
+		float hx = px;
+		float hy = py;
+		float aTan=-1/tan(ra);
+		if (ra>PI)  //looking down
+		{
+			ry = (((int)py>>6)<<6)-0.0001;
+			rx = (py-ry)*aTan + px;
+			yo = -64;
+			xo = -yo*aTan;
+		}
+		if (ra<PI) //looking up
+		{
+			ry = (((int)py>>6)<<6)+64;
+			rx = (py-ry)*aTan + px;
+			yo = 64;
+			xo = -yo*aTan;
+		}
+		if (ra==0||ra==PI)
+		{
+			rx=px;
+			ry=py;
+			dof = 8;
+		}
+		
+		while(dof<8)
+		{
+  			mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;  
+			if (mp<0 && mp<mapX*mapY && map[mp]==1)//hit wall
+			{
+				dof = 8;
+				hx = rx;
+				hy = ry;
+				disH = dist(px,py,hx,hy,ra);
+			}
+			else
+			{
+				rx+=xo;
+				ry+=yo;
+				dof+=1;
+			}
+			
+			glColor3f(0,1,0);
+			glLineWidth(10);
+			glBegin(GL_LINES);
+			glVertex2i(px,py);
+			glVertex2i(rx,ry);
+			glEnd();
+		}
+		
+		
+		//----------------------Check Vertical Lines----------------------
+		dof = 0;
+		float disV = 1547800;
+		float vx = px;
+		float vy = py;
+		float nTan=-tan(ra);
+		if (ra>P2 && ra<P3) //looking left
+		{
+			rx = (((int)px>>6)<<6)-0.0001;
+			ry = (px-rx)*nTan + py;
+			xo = -64;
+			yo = -xo*nTan;
+		}
+		if (ra<P2 || ra>P3)//looking right
+		{
+			rx = (((int)px>>6)<<6)-+64;
+			ry = (px-rx)*nTan + py;
+			xo = 64;
+			yo = -xo*nTan;
+		}
+		if (ra==0||ra==PI)
+		{
+			rx=px;
+			ry=py;
+			dof = 8;
+		}
+		
+		while(dof<8)
+		{
+			mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx; 
+			if (mp>0 && mp<mapX*mapY && map[mp]==1)//hit wall
+			{
+				dof = 8;
+				vx = rx;
+				vy = ry;
+				disV = dist(px,py,vx,vy,ra);
+			}
+			else
+			{
+				rx+=xo;
+				ry+=yo;
+				dof+=1;
+			}
+			
+			if (disV<disH)
+			{
+				rx = vx;
+				ry = vy;
+			}
+			
+			if (disH<disV)
+			{
+				rx = hx;
+				ry = hy;	
+			}
+			
+			glColor3f(1,0,0);
+			glLineWidth(3);
+			glBegin(GL_LINES);
+			glVertex2i(px,py);
+			glVertex2i(rx,ry);
+			glEnd();
+		}
+	}
+}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	drawMap2D();
 	drawPlayer();
+	drawRays3D();
 	glutSwapBuffers();
 }
 
 void buttons(unsigned char key, int x, int y)
 {
-	if (key=='a'){px -=5;}
-	if (key=='d'){px +=5;}
-	if (key=='w'){py -=5;}
-	if (key=='s'){py +=5;}
+	if (key=='a')
+	{
+		pa-=.1; 
+		if (pa < 0)
+			pa+=2*PI;
+		pdx = cos(pa)*5;
+		pdy = sin(pa)*5;
+	}
+	if (key=='d')
+	{		
+		pa+=.1; 
+		if (pa > 2*PI)
+			pa-=2*PI;
+		pdx = cos(pa)*5;
+		pdy = sin(pa)*5;
+		
+	}
+	if (key=='w')
+	{
+		px+=pdx;
+		py+=pdy;
+	}
+	if (key=='s')
+	{
+		px-=pdx;
+		py-=pdy;
+	}
 	glutPostRedisplay();
 }
 
@@ -81,6 +247,8 @@ void init()
 	glClearColor(.3,.3,.3,0);
 	gluOrtho2D(0,1024,512,0);
 	px = 300; py = 300;
+	pdx = cos(pa)*5;
+	pdy = sin(pa)*5;
 }
 
 int main(int argc, char** argv)
