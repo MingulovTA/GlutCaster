dd691d3d0450411f18f481748f0bbbe876f010f9
DrTressi <DrTressi@yandex.ru>
2025-08-23 01:34:29 +0700

    feat: create simple map example

--- GlutCaster.c
+++ GlutCaster.c
@@ -16,9 +16,53 @@ void drawPlayer()
 	glEnd();
 }
 
+int mapX = 8, mapY = 8, mapS = 64;
+
+int map[] = 
+{
+	1,1,1,1,1,1,1,1,
+	1,0,1,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,1,0,0,0,0,1,
+	1,0,0,0,0,0,0,1,
+	1,0,0,0,1,0,0,1,
+	1,0,0,0,1,0,0,1,
+	1,1,1,1,1,1,1,1,
+};
+
+void drawMap2D()
+{
+	int x,y,xo,yo;
+	for(y=0;y<mapY;y++)
+	{
+		for (x=0;x<mapX;x++)
+		{
+			if (map[y*mapX+x]==1)
+			{
+				glColor3d(1,1,1);
+			}
+			else
+			{
+				glColor3f(0,0,0);
+			}
+			xo = x*mapS; 
+			yo = y*mapS;
+			
+			glBegin(GL_QUADS);
+			glVertex2i(xo,yo);
+			glVertex2i(xo,yo+mapS);
+			glVertex2i(xo+mapS,yo+mapS);
+			glVertex2i(xo+mapS,yo);
+			glEnd();
+			
+		}
+	}
+}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	drawMap2D();
 	drawPlayer();
 	glutSwapBuffers();
 }
